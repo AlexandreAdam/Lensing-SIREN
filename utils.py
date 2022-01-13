1cc4acb9da340d3a7caa008a8e8064d3a01b3d28
Alexandre Adam <adam.alexandre01213@gmail.com>
2022-01-13 14:07:12 -0500

    added maml scripts from Tristan Deleu

---
+++ utils.py
@@ -0,0 +1,59 @@
+"""
+MIT License
+
+Copyright (c) 2019 Tristan Deleu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import torch
+
+from collections import OrderedDict
+from torchmeta.modules import MetaModule
+
+
+def compute_accuracy(logits, targets):
+    """Compute the accuracy"""
+    with torch.no_grad():
+        _, predictions = torch.max(logits, dim=1)
+        accuracy = torch.mean(predictions.eq(targets).float())
+    return accuracy.item()
+
+def tensors_to_device(tensors, device=torch.device('cpu')):
+    """Place a collection of tensors in a specific device"""
+    if isinstance(tensors, torch.Tensor):
+        return tensors.to(device=device)
+    elif isinstance(tensors, (list, tuple)):
+        return type(tensors)(tensors_to_device(tensor, device=device)
+            for tensor in tensors)
+    elif isinstance(tensors, (dict, OrderedDict)):
+        return type(tensors)([(name, tensors_to_device(tensor, device=device))
+            for (name, tensor) in tensors.items()])
+    else:
+        raise NotImplementedError()
+
+class ToTensor1D(object):
+    """Convert a `numpy.ndarray` to tensor. Unlike `ToTensor` from torchvision,
+    this converts numpy arrays regardless of the number of dimensions.
+    Converts automatically the array to `float32`.
+    """
+    def __call__(self, array):
+        return torch.from_numpy(array.astype('float32'))
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
